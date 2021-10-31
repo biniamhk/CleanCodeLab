d7be32feb4d9de570c461c4b1b15d506e00d86fd
Samir79 <samirovsky79@gmail.com>
2021-10-31 15:31:48 +0100

    LifeTest-class: Testing countNeighbours-method

--- src/main/java/se/cleancode/lab1/Life.java
+++ src/main/java/se/cleancode/lab1/Life.java
@@ -3,15 +3,35 @@ package se.cleancode.lab1;
 public class Life {
     public int initializeBoard(Board board) {
         int numberOfLivingCells = 0;
+
         for (int row = 0; row < board.getRows(); row++) {
             for (int col = 0; col < board.getColumns(); col++) {
-                int random = (int)(Math.random()*3);
-                if(random==0){
-                    board.setValue(row, col,1);
+                int random = (int) (Math.random() * 3);
+                if (random == 0) {
+                    board.setValue(row, col, 1);
                     numberOfLivingCells++;
+                }
+
+            }
+
+        }
+        return numberOfLivingCells;
+    }
+
+    public int countNeighbours(int row, int col, Board board) {
+
+        int count = 0;
+        for (int r = row - 1; r <= row + 1; r++) {
+            for (int c = col - 1; c <= col + 1; c++) {
+                if (c >= 0 && c < board.getColumns() &&
+                        r >= 0 && r < board.getRows() &&
+                        !(r == row && c == col) &&
+                        board.getValueAtPosition(r, c) == 1) {
+                    count=count+1;
                 }
             }
         }
-        return numberOfLivingCells;
+
+          return count;
     }
 }
--- src/test/java/se/cleancode/lab1/LifeTest.java
+++ src/test/java/se/cleancode/lab1/LifeTest.java
@@ -3,7 +3,6 @@ package se.cleancode.lab1;
 import org.junit.jupiter.api.Test;
 
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -13,14 +12,32 @@ public class LifeTest {
     //Iinitialize the grid using math-random method
 
     Board board;
-    Life life =new Life();
+    Life life;
+
     @Test
     void checkIfValuesAreAssignedToCells() {
         board = new Board(3, 4);
+        life= new Life();
         int livingCells = life.initializeBoard(board);
 
-        assertTrue(livingCells>0);
+        assertTrue(livingCells > 0);
 
     }
+
+    @Test
+    void checkNumberOfLivingNeighbours() {
+        board = new Board(3, 4);
+        life= new Life();
+
+        board.setValue(1,1,1);
+        board.setValue(0,0,1);
+        board.setValue(0,1,1);
+        board.setValue(2,2,1);
+        board.setValue(1,2,1);
+
+        assertEquals(4,life.countNeighbours(1,1,board));
+
+    }
+
 
 }
